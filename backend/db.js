4355eae1b40cd75b979fc2b828238670e5d24323
creideiki <k.creideiki@gmail.com>
2026-08-13 16:09:57 +0300

    Connect backend to Neon

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,30 @@
-const { Pool } = require("pg"); 
+/*const { Pool } = require("pg");
 
-const pool = new Pool({ 
+const pool = new Pool({
+  user: "postgres",
 
-  user: "postgres", 
+  host: "127.0.0.1",
 
-  host: "127.0.0.1", 
+  database: "diary_db",
 
-  database: "diary_db", 
+  password: "1234",
+  port: 5432,
+}); */
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-  password: "1234",       
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Ошибка подключения к БД', err.stack);
+  }
 
-  port: 5432, 
+  console.log('Подключено к PostgreSQL');
 
-}); 
-pool.connect((err, client, release) => { 
-
-  if (err) { 
-
-    return console.error("Ошибка подключения к БД", err.stack); 
-
-  } 
-
-  console.log("Подключено к PostgreSQL"); 
-
-  release(); 
-
-}); 
-module.exports = pool; 
+  release();
+});
+module.exports = pool;
